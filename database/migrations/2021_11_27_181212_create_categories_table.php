01ab4079ab736e510f654e057bca76defdb9249f
rogerBret <rogebertrand360]@gmail.com>
2021-11-30 11:23:34 +0100

    mise à jour
    mise à jour

--- database/migrations/2021_11_27_181212_create_categories_table.php
+++ database/migrations/2021_11_27_181212_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->sting('libele');
+            $table->string('libele');
             $table->string('image')->nullable();
             $table->timestamps();
         });
